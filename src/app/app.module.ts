3c8960aba8feb4f23089fcc303b98c333ea0d7a6
davigadelha <davi.gadelha@gmail.com>
2020-01-08 15:36:22 -0300

    Implementação dos componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,21 +2,53 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
 import { RouterModule } from '@angular/router';
-
+import {ROUTES} from './app.routes';
 
 import { AppComponent } from './app.component'
-
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { OrderComponent } from './order/order.component';
+import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component';
+import { MenuComponent } from './restaurant-detail/menu/menu.component';
+import { RestaurantsComponent } from './restaurants/restaurants.component';
+import { RestaurantDetailComponent } from './restaurant-detail/restaurant-detail.component';
+import { FormsModule } from '@angular/forms';
+import { OrderItemsComponent } from './order/order-items/order-items.component';
+import { InputComponent } from './shared/input/input.component';
+import { RadioComponent } from './shared/radio/radio.component';
+import { MenuItemComponent } from './restaurant-detail/menu-item/menu-item.component';
+import { ShoppingCartComponent } from './restaurant-detail/shopping-cart/shopping-cart.component';
+import { RestaurantComponent } from './restaurants/restaurant/restaurant.component';
+import { RestaurantsService } from './restaurants/restaurants.service';
+import { ShoppingCartService } from './restaurant-detail/shopping-cart/shopping-cart.service';
+import { OrderService } from './order/order.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    AboutComponent,
+    OrderComponent,
+    OrderItemsComponent,
+    ReviewsComponent,
+    MenuComponent,
+    MenuItemComponent,
+    RestaurantsComponent,
+    RestaurantComponent,
+    RestaurantDetailComponent,
+    InputComponent,
+    RadioComponent,
+    ShoppingCartComponent
   ],
   imports: [
     BrowserModule,
     HttpModule,
-    RouterModule
+    FormsModule,
+    RouterModule.forRoot(ROUTES)
   ],
-  providers: [],
+  providers: [RestaurantsService, ShoppingCartService, OrderService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
