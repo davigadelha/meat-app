d73b3c03d0c4344dcebb34e8efb216d9f9452bd4
Davi Augusto Gadêlha Silva <davi-augusto.silva@serpro.gov.br>
2020-03-09 16:58:02 -0300

    Utilização do canActivate

--- src/app/security/login/loggedin.guard.ts
+++ src/app/security/login/loggedin.guard.ts
@@ -1,17 +1,25 @@
 import { LoginService } from './login.service';
-import { CanLoad, Route } from "@angular/router";
+import { CanLoad, Route, ActivatedRouteSnapshot, RouterStateSnapshot, CanActivate } from "@angular/router";
 import { Injectable } from "@angular/core";
 
 @Injectable()
-export class LoggedInGuard implements CanLoad {
+export class LoggedInGuard implements CanLoad, CanActivate {
 
-    constructor(private loginService: LoginService){}
+    constructor(private loginService: LoginService) {}
 
-    canLoad(route: Route): boolean {
+    checkAuthentication(path: string): boolean {
         const loggedIn = this.loginService.isLoggedIn();
-        if (!loggedIn){
-            this.loginService.handleLogin(`/${route.path}`);
+        if (!loggedIn) {
+            this.loginService.handleLogin(`/${path}`);
         }
         return loggedIn;
+    }
+
+    canLoad(route: Route): boolean {
+        return this.checkAuthentication(route.path);
+    }
+
+    canActivate(activatedRoute: ActivatedRouteSnapshot, routerState: RouterStateSnapshot): boolean{
+        return this.checkAuthentication(activatedRoute.routeConfig.path);
     }
 }
