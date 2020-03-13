a6a68312cbe11deda29f3a9f5c3f9f02dc989971
Davi Augusto Gadêlha Silva <davi-augusto.silva@serpro.gov.br>
2020-03-13 14:34:38 -0300

    Login manual

--- src/app/security/login/login.service.ts
+++ src/app/security/login/login.service.ts
@@ -1,4 +1,4 @@
-import { Router } from '@angular/router';
+import { Router, NavigationEnd } from '@angular/router';
 import { User } from './user.model';
 import { MEAT_API } from './../../app.api';
 import { Observable } from 'rxjs/Observable';
@@ -6,12 +6,18 @@ import 'rxjs/add/operator/do';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
+import 'rxjs/add/operator/filter';
+
 @Injectable()
 export class LoginService {
 
     user: User;
+    lastUrl: string;
 
-    constructor(private http: HttpClient, private router: Router) {}
+    constructor(private http: HttpClient, private router: Router) {
+        this.router.events.filter(e => e instanceof NavigationEnd)
+                          .subscribe( (e: NavigationEnd) => this.lastUrl = e.url);
+    }
 
     isLoggedIn(): boolean {
         console.log('isLoggedIn: ' + this.user !== undefined);
@@ -24,7 +30,11 @@ export class LoginService {
                     .do(user => this.user = user);
     }
 
-    handleLogin(path: string) {
+    handleLogin(path: string = this.lastUrl) {
         this.router.navigate(['/login', btoa(path)]);
+    }
+
+    logout() {
+        this.user = undefined;
     }
 }
